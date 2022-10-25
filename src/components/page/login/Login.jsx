61c205ad5f9d5b7941b01e55febe72ffa447e27f
Jahidul Islam <jahid2111@gmail.com>
2022-10-25 22:45:09 +0600

    signUp page design

--- src/components/page/login/Login.jsx
+++ src/components/page/login/Login.jsx
@@ -21,13 +21,13 @@ const Login = () => {
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
                                 <label className="label">
                                     <Link to="#" className="label-text-alt link link-hover">Forgot password?</Link>
                                 </label>
--- src/components/page/signUp/SignUp.jsx
+++ src/components/page/signUp/SignUp.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
     return (
         <div>
-            <h1>signUp</h1>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col w-full max-w-xl">
+                    <div className="text-center">
+                        <h1 className="text-5xl font-bold">Create New Account!</h1>
+                        <p className="py-6">please create your learn English Course account</p>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
+                        <form className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="Write your Name!" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">photo URL</span>
+                                </label>
+                                <input type="text" name='photURL' placeholder="Drop Your Photo URL" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' placeholder="a@email.com" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <Link to="#" className="label-text-alt link link-hover">Forgot password?</Link>
+                                </label>
+                            </div>
+                            <div className="mt-6 text-center">
+                                <button className="btn btn-primary lg:w-56">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
